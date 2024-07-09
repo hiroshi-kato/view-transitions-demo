1dff532b30e7786d24ed9c75446d9f815be70417
mamedaifuku <hiroshikato1208@gmail.com>
2024-07-09 09:58:15 +0900

    記事ヘッダーのトップ画面への遷移をstartViewTransitionを使ったbuttonに変更

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,11 +1,18 @@
-import Link from "next/link";
+'use client';
+
+import { startViewTransition } from '@/lib/startViewTransition';
+import { useViewTransitionsRouter } from '@/lib/useViewTransitionsRouter';
+import { useRouter } from 'next/navigation';
 
 const Header = () => {
+  const router = useRouter();
+  const onClick = () => startViewTransition(() => router.push('/'));
+
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 flex items-center">
-      <Link href="/" className="hover:underline">
+    <h2 className='text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 flex items-center'>
+      <button onClick={onClick} className='hover:underline'>
         Blog
-      </Link>
+      </button>
       .
     </h2>
   );
