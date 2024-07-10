2b82481c4abd8ace56cd75b0b8d263381d49b337
hiroshi-kato <hiroshikato1208@gmail.com>
2024-07-10 09:44:08 +0900

    feat: Update page title and description for View Transitions API demo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
+  title: `View Transitions API demo with Next.js blog starter`,
   description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
   openGraph: {
     images: [HOME_OG_IMAGE_URL],
